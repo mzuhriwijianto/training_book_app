aefe924f4c4012ec22dca449204ccf4f28a7c5be
5t4r-h4rf4 <bp0250@gmail.com>
2021-12-02 15:16:46 +0700

    tombol

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -38,7 +38,7 @@
                                 <td>{{$category->created_at_with_format}}</td>
                                 <td>{{$category->updated_at_with_format}}</td>
                                 <td class="center">
-                                    <a href="{{route('category.edit', ['id' => $category->id])}}" class="btn btn-primary">Edit</a>
+                                    <a href="{{route('category.edit', ['id' => $category->id])}}" class="btn btn-danger">Edit</a>
                                     @component('components.delete')
                                     @slot('url')
                                         {{ route('category.delete', ['id' => $category->id]) }}
